fc16915da0a21a415bbd9f27aefea65a96a716df
Daksh Daga <dagarkdaksh@gmail.com>
2026-08-02 15:37:36 +0530

    Time: 422 ms (14.53%), Space: 136.8 MB (22.56%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,3 +1,15 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        memo = {}
+
+        def helper(left, right):
+            if(left == right): return piles[left]
+            if (left, right) in memo: return memo[(left, right)]
+
+            take_left = piles[left] - helper(left+1, right)
+            take_right = piles[right] - helper(left, right-1)
+
+            memo[(left, right)] = max(take_left, take_right)
+            return memo[(left, right)]
+        
+        return helper(0, len(piles)-1) >= 0
