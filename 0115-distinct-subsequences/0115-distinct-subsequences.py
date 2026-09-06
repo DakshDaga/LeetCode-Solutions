4bd3627825e2a2b6f6e934ea22edf7f3b10ad79a
Daksh Daga <dagarkdaksh@gmail.com>
2026-09-06 10:23:11 +0530

    Time: 1824 ms (5%), Space: 139.9 MB (21.93%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,15 +1,15 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         memo = [[-1 for _ in range(1001)] for _ in range(1001)]
-        def solve(s, t, i, j):
+        def solve(i, j):
             if(j == len(t)): return 1
             if(i == len(s)): return 0
 
             if memo[i][j] != -1: return memo[i][j]
             if(s[i] == t[j]):
-                memo[i][j] = solve(s, t, i+1, j+1) + solve(s, t, i+1, j)
+                memo[i][j] = solve(i+1, j+1) + solve(i+1, j)
 
-            else: memo[i][j] = solve(s, t, i+1, j)
+            else: memo[i][j] = solve(i+1, j)
             return memo[i][j]
         
-        return solve(s, t, 0, 0)
+        return solve(0, 0)
