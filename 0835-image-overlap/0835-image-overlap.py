61c32adadeaa12fe9991fad27df8f769cb1426ba
Daksh Daga <dagarkdaksh@gmail.com>
2026-09-13 11:03:49 +0530

    Time: 219 ms (67.72%), Space: 19.7 MB (50%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.py
+++ 0835-image-overlap/0835-image-overlap.py
@@ -2,18 +2,19 @@ class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
         possible = {}
         n = len(img1)
+        pos_of_1s_img2 = []
 
-        def get_diff(i, j):
-            for x in range(n):
-                for y in range(n):
-                    if img2[x][y] == 0: continue
-                    row_diff = x-i
-                    col_diff = y-j
-                    possible[(row_diff, col_diff)] = possible.get((row_diff, col_diff), 0) + 1 
+        for x in range(n):
+            for y in range(n):
+                if img2[x][y] == 1:
+                    pos_of_1s_img2.append([x, y])
 
         for i in range(n):
             for j in range(n):
                 if img1[i][j] == 0: continue
-                get_diff(i, j)
+                for pos in pos_of_1s_img2:
+                    row_diff = i - pos[0]
+                    col_diff = j - pos[1]
+                    possible[(row_diff, col_diff)] = possible.get((row_diff, col_diff), 0) + 1
         
         return max(possible.values(), default=0)
