751b16d01e1e4d950da6fd6faf8cf4dd96a6728a
Daksh Daga <dagarkdaksh@gmail.com>
2026-09-26 08:21:32 +0530

    Time: 42 ms (83.8%), Space: 51.4 MB (61.68%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
@@ -1,18 +1,17 @@
 class Solution:
     def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
-        n = len(s)
         mp = dict(knowledge)
         ans = ""
         i = 0
-        while i<n: 
+        while i < len(s): 
             if s[i] == '(':
                 key = ""
                 i += 1
                 while s[i] != ')':
                     key += s[i]
-                    i += 1
-            
+                    i += 1          
                 ans += mp.get(key, "?")
+
             else:
                 ans += s[i]
             i += 1
